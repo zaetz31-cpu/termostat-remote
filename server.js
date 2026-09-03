75d09167673c7854d7d4c8ec21bccbf0e1c35358
zaetz31-cpu <zaetz31@gmail.com>
2026-09-03 17:54:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,50 +5,39 @@ const WebSocket = require("ws");
 const app = express();
 const server = http.createServer(app);
 
-const wss = new WebSocket.Server({ server });
+const wss = new WebSocket.Server({ noServer: true });
 
 const PORT = process.env.PORT || 10000;
 
-let shelly = null;
+server.on("upgrade", (req, socket, head) => {
+  console.log(">>> WEBSOCKET UPGRADE:", req.url);
 
-console.log("================================");
-console.log("TERMOSTAT REMOTE");
-console.log("PORT:", PORT);
-console.log("WS: ORICE PATH");
-console.log("================================");
+  wss.handleUpgrade(req, socket, head, (ws) => {
+    console.log(">>> WEBSOCKET CONECTAT:", req.url);
+
+    wss.emit("connection", ws, req);
+  });
+});
 
 wss.on("connection", (ws, req) => {
-
-  console.log("================================");
-  console.log("!!! SHELLY CONECTAT !!!");
+  console.log(">>> SHELLY CONECTAT !!!");
   console.log("PATH:", req.url);
-  console.log("IP:", req.socket.remoteAddress);
-  console.log("================================");
-
-  shelly = ws;
 
   ws.on("message", data => {
     console.log("SHELLY ->", data.toString());
   });
 
   ws.on("close", (code, reason) => {
-    console.log("SHELLY DISCONNECTED:", code, reason.toString());
-    if (shelly === ws) shelly = null;
+    console.log(">>> SHELLY DISCONNECTED:", code, reason.toString());
   });
 
   ws.on("error", err => {
-    console.log("WS ERROR:", err.message);
+    console.log(">>> WS ERROR:", err.message);
   });
-
-  ws.send(JSON.stringify({
-    id: 1,
-    src: "termostat-remote",
-    method: "Shelly.GetStatus"
-  }));
 });
 
 app.get("/", (req, res) => {
-  res.send("TERMOSTAT REMOTE - OK");
+  res.send("TERMOSTAT REMOTE OK");
 });
 
 app.get("/shelly", (req, res) => {
@@ -56,5 +45,5 @@ app.get("/shelly", (req, res) => {
 });
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("SERVER GATA");
+  console.log("SERVER GATA PE PORT:", PORT);
 });
